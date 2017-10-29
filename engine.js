de2a18c9d8a15f4be29d41da1dacfa253840abcb
Stephen Muiruri <dig3nius@users.noreply.github.com>
2017-10-29 23:07:05 +0300

    Diverge
    scatter

---
+++ engine.js
@@ -0,0 +1,4 @@
+/*this is where everything converges and data is interacted, blended together and merged with other informations to create interesting new finds
+to be used by the entire application for correlation of data.
+
+**/
